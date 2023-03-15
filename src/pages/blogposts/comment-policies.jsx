d18d4875104c8255c7e4fef6d8a44df3a5336840
naufalHaidar12342 <111201912342@mhs.dinus.ac.id>
2023-03-15 21:19:48 +0700

    add comment-policies page for disqus

---
+++ src/pages/blogposts/comment-policies.jsx
@@ -0,0 +1,9 @@
+import Layout from "@/components/Layout";
+
+export default function CommentPolicies() {
+	return (
+		<Layout pageTitle="naufalHaidar12342's Comment Policies">
+			<div></div>
+		</Layout>
+	);
+}
