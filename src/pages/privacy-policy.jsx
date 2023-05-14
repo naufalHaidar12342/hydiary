773f07d3df517823819160275ce9cac2d0bfd979
naufalHaidar12342 <111201912342@mhs.dinus.ac.id>
2023-05-14 17:24:07 +0700

    replace footer style, add dark mode color from github and brave browser, add privacy policy page, add content to project page

---
+++ src/pages/privacy-policy.jsx
@@ -0,0 +1,178 @@
+import Layout from "@/components/Layout";
+
+export default function PrivacyPolicy() {
+	return (
+		<Layout pageTitle="Privacy Policy">
+			<div className="min-h-screen max-w-screen-lg mx-auto">
+				<div className="flex flex-col justify-center items-center">
+					<h1 className="font-extrabold font-mono text-2xl mt-4">
+						Privacy Policy for naufalHaidar12342
+					</h1>
+					<div className="my-4" aria-label="Policies">
+						<div className="mb-10">
+							<p className="mt-2">
+								At naufalHaidar12342, accessible from naufalhaidar12342.cyou,
+								one of our main priorities is the privacy of our visitors. This
+								Privacy Policy document contains types of information that is
+								collected and recorded by naufalHaidar12342 and how we use it.
+							</p>
+
+							<p>
+								If you have additional questions or require more information
+								about our Privacy Policy, do not hesitate to contact us.
+							</p>
+						</div>
+						<div className="mb-10">
+							<h2 className="font-bold text-lg">Analytics</h2>
+							<p className="mt-4">
+								naufalHaidar12342 use "Web Analytics" service, provided by this
+								website hosting service, Vercel. This service is used to collect
+								and analyze the following information. Note that these
+								information does not specify visitor as an individual.
+							</p>
+							<div className="ltr">
+								<ul
+									className="block list-disc"
+									style={{
+										marginBlockStart: "1em",
+										marginBlockEnd: "1em",
+										marginInlineStart: "0px",
+										marginInlineEnd: "0px",
+										paddingInlineStart: "40px",
+									}}
+								>
+									<li>number of visitors,</li>
+									<li>most visited pages,</li>
+									<li>visitor's country of origin,</li>
+									<li>browser used by visitor,</li>
+									<li>operating system used by visitor, and</li>
+									<li>number of page views.</li>
+								</ul>
+							</div>
+						</div>
+						<div className="mb-10">
+							<h2 className="font-bold text-lg">Log Files</h2>
+
+							<p className="mt-4">
+								naufalHaidar12342 follows a standard procedure of using log
+								files. These files log visitors when they visit websites. All
+								hosting companies do this and a part of hosting services'
+								analytics. The information collected by log files include
+								internet protocol (IP) addresses, browser type, Internet Service
+								Provider (ISP), date and time stamp, referring/exit pages, and
+								possibly the number of clicks. These are not linked to any
+								information that is personally identifiable. The purpose of the
+								information is for analyzing trends, administering the site,
+								tracking users' movement on the website, and gathering
+								demographic information. Our Privacy Policy was created with the
+								help of the{" "}
+								<a
+									href="https://www.termsfeed.com/privacy-policy-generator/"
+									className="link-hover"
+								>
+									Privacy Policy Generator
+								</a>
+								.
+							</p>
+						</div>
+						<div className="mb-10">
+							<h2 className="font-bold text-lg">Cookies and Web Beacons</h2>
+
+							<p className="mt-4">
+								Like any other website, naufalHaidar12342 uses "cookies". These
+								cookies are used to store information including visitors'
+								preferences, and the pages on the website that the visitor
+								accessed or visited. The information is used to optimize the
+								users' experience by customizing our web page content based on
+								visitors' browser type and/or other information.
+							</p>
+						</div>
+						<div className="mb-10">
+							<h2 className="font-bold text-lg">Privacy Policies</h2>
+
+							<p className="mt-4">
+								You may consult this list to find the Privacy Policy for each of
+								the advertising partners of naufalHaidar12342.
+							</p>
+
+							<p className="mt-4">
+								Third-party ad servers or ad networks uses technologies like
+								cookies, JavaScript, or Web Beacons that are used in their
+								respective advertisements and links that appear on
+								naufalHaidar12342, which are sent directly to users' browser.
+								They automatically receive your IP address when this occurs.
+								These technologies are used to measure the effectiveness of
+								their advertising campaigns and/or to personalize the
+								advertising content that you see on websites that you visit.
+							</p>
+
+							<p className="mt-4">
+								Note that naufalHaidar12342 has no access to or control over
+								these cookies that are used by third-party advertisers.
+							</p>
+						</div>
+						<div className="mb-10">
+							<h2 className="font-bold text-lg">
+								Third Party Privacy Policies
+							</h2>
+
+							<p className="mt-4">
+								naufalHaidar12342's Privacy Policy does not apply to other
+								advertisers or websites. Thus, we are advising you to consult
+								the respective Privacy Policies of these third-party ad servers
+								for more detailed information. It may include their practices
+								and instructions about how to opt-out of certain options.{" "}
+							</p>
+
+							<p className="mt-4">
+								You can choose to disable cookies through your individual
+								browser options. To know more detailed information about cookie
+								management with specific web browsers, it can be found at the
+								browsers' respective websites. What Are Cookies?
+							</p>
+						</div>
+						<div className="mb-10">
+							<h2 className="font-bold text-lg">Children's Information</h2>
+
+							<p className="mt-4">
+								Another part of our priority is adding protection for children
+								while using the internet. We encourage parents and guardians to
+								observe, participate in, and/or monitor and guide their online
+								activity.
+							</p>
+
+							<p className="mt-4">
+								naufalHaidar12342 does not knowingly collect any Personal
+								Identifiable Information from children under the age of 13. If
+								you think that your child provided this kind of information on
+								our website, we strongly encourage you to contact us immediately
+								and we will do our best efforts to promptly remove such
+								information from our records.
+							</p>
+						</div>
+						<div className="mb-10">
+							<h2 className="font-bold text-lg">Online Privacy Policy Only</h2>
+
+							<p>
+								This Privacy Policy applies only to our online activities and is
+								valid for visitors to our website with regards to the
+								information that they shared and/or collect in
+								className="font-bold text-lg" naufalHaidar12342. This policy is
+								not applicable to any information collected offline or via
+								channels other than this website.
+							</p>
+						</div>
+						<div className="mb-10">
+							<h2>Consent</h2>
+
+							<p className="mt-4">
+								By using our website, you hereby consent to our Privacy Policy
+								and agree to its Terms and Conditions.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</Layout>
+	);
+}
