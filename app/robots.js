dd970a97a146ecbbf368051113f58039ac2e01df
naufalHaidar12342 <111201912342@mhs.dinus.ac.id>
2023-09-19 20:42:06 +0700

    add opengraph following @vercel/og docs

--- app/robots.js
+++ app/robots.js
@@ -2,7 +2,7 @@ export default function robots() {
 	return {
 		rules: {
 			userAgents: "*",
-			allow: "/",
+			allow: ["/", "/og/*"],
 		},
 		sitemap: "https://naufalhaidar12342.cyou/sitemap.xml",
 	};
