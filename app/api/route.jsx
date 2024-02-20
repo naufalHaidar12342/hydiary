21dde9a68696303658eac8b2b34654471830ee25
naufalHaidar12342 <naufalhaidar12342@gmail.com>
2024-02-20 15:47:27 +0700

    disable GET request for resend api

--- app/api/route.jsx
+++ app/api/route.jsx
@@ -1,13 +1,13 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-export async function GET() {
-	const resend = new Resend(process.env.RESEND_API_KEY);
-	try {
-		// const { data } = await resend.emails.send({});
-		return NextResponse.json({ message: "Hello World" });
-	} catch (error) {
-		console.error(error);
-	}
-	return NextResponse.json({ message: "Hello World" });
-}
+// export async function GET() {
+// 	const resend = new Resend(process.env.RESEND_API_KEY);
+// 	try {
+// 		// const { data } = await resend.emails.send({});
+// 		return NextResponse.json({ message: "Hello World" });
+// 	} catch (error) {
+// 		console.error(error);
+// 	}
+// 	return NextResponse.json({ message: "Hello World" });
+// }
